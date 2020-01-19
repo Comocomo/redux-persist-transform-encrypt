357ed6ffd63333241b517c1571d51862618609a7
Hila Raveh <hila@nutrinohealth.com>
2020-01-19 11:35:49 +0200

    add immutable

--- src/sync.js
+++ src/sync.js
@@ -1,17 +1,30 @@
 import { createTransform } from 'redux-persist'
 import CryptoJSCore from 'crypto-js/core'
 import AES from 'crypto-js/aes'
+import R from 'ramda'
+import Immutable from 'seamless-immutable'
 import { makeEncryptor, makeDecryptor } from './helpers'
 
+// is this object already Immutable?
+const isImmutable = R.has('asMutable')
+
+// change this Immutable object into a JS object
+const convertToJs = (state) => state.asMutable({deep: true})
+
+// optionally convert this object into a JS object if it is Immutable
+const fromImmutable = R.when(isImmutable, convertToJs)
+
+const toImmutable = (raw) => Immutable(raw)
+
 const makeSyncEncryptor = secretKey =>
-  makeEncryptor(state => AES.encrypt(state, secretKey).toString())
+  makeEncryptor(state => AES.encrypt(fromImmutable(state), secretKey).toString())
 
 const makeSyncDecryptor = (secretKey, onError) =>
   makeDecryptor(state => {
     try {
-      const bytes = AES.decrypt(state, secretKey)
-      const decryptedString = bytes.toString(CryptoJSCore.enc.Utf8)
-      return JSON.parse(decryptedString)
+        const bytes = AES.decrypt(state, secretKey)
+        const decryptedString = bytes.toString(CryptoJSCore.enc.Utf8)
+        return toImmutable(JSON.parse(decryptedString))
     } catch (err) {
       throw new Error(
         'Could not decrypt state. Please verify that you are using the correct secret key.'
